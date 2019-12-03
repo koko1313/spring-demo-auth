aa2a9211580931151c9e06d497dc431b5ca655fa
koko1313 <kaloyanvellichkov@gmail.com>
2019-12-03 11:40:59 +0200

    in HomeController added some end-points with authorization examples

--- src/main/java/com/example/springdemoauth/rest/HomeController.java
+++ src/main/java/com/example/springdemoauth/rest/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.springdemoauth.rest;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@GetMapping("/")
-	public String index() {
-		return "Hey";
+	public String everyone() {
+		return "You can reach this page because you are not authenticated!";
+	}
+	
+	@GetMapping("/admin")
+	@Secured("ROLE_ADMIN")
+	public String admin(Authentication authentication) {
+		return "Hey! You see this page because you have role admin. Your username is " + authentication.getName();
+	}
+	
+	@GetMapping("/user")
+	@Secured("ROLE_USER")
+	public String user() {
+		return "Heyyy you user!";
 	}
 	
 }
