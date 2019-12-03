b81ce39921bfe62158b0511a56d333fc6dad0d07
koko1313 <kaloyanvellichkov@gmail.com>
2019-12-03 11:39:14 +0200

    created LoginController

---
+++ src/main/java/com/example/springdemoauth/rest/LoginController.java
@@ -0,0 +1,62 @@
+package com.example.springdemoauth.rest;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springdemoauth.models.UserModel;
+
+@RestController
+public class LoginController {
+
+	private UserDetailsService userDetailsService;
+	
+	public LoginController(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+	
+	// have to be with PostMapping
+	@GetMapping(path = "/login")
+	public boolean login(
+			@RequestParam(value = "username") String username, 
+			@RequestParam(value = "password") String password) {
+		
+		// UserModel user = userRepo.findUserByUsernameAndPassword(username, password);
+		
+		// hardcored
+		UserModel user = new UserModel();
+		user.setUsername(username);
+		// end of hardcored
+		
+		if(user != null) {
+			UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+			
+			if(userDetails != null) {
+				Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+			
+			return true;
+		}
+			
+		return false;
+	}
+	
+	// have to be with PostMapping
+	@GetMapping(path = "/logout-user")
+	public boolean logout(HttpSession session) {
+		session.invalidate();
+		
+		return true;
+	}
+
+}
+
+
